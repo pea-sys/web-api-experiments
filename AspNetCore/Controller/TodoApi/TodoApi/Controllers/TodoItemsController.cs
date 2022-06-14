08a508c0793d5b327af4cf6bf23124043e97ee73
pea-sys <pea98258@gmail.com>
2022-06-14 22:13:38 +0900

    JavaScript Called Web API

--- AspNetCore/Controller/TodoApi/TodoApi/Controllers/TodoItemsController.cs
+++ AspNetCore/Controller/TodoApi/TodoApi/Controllers/TodoItemsController.cs
@@ -50,7 +50,7 @@ namespace TodoApi.Controllers
         // PUT: api/TodoItems/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateTodoItem(long id, TodoItemDTO todoItem)
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
         {
             if (id != todoItem.Id)
             {
--- AspNetCore/Controller/TodoApi/TodoApi/Program.cs
+++ AspNetCore/Controller/TodoApi/TodoApi/Program.cs
@@ -20,7 +20,11 @@ if (app.Environment.IsDevelopment())
 {
     //app.UseSwagger();
     //app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 

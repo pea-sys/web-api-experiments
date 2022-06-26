fe7312862341d30feb49ea75931f1178490b196b
pea-sys <pea98258@gmail.com>
2022-06-26 09:22:24 +0900

    SQL Serverの分散キャッシュ

---
+++ AspNetCore/DistributedCache/DistributedCache/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace DistributedCache.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly IDistributedCache _cache;
+        public IndexModel(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public string? CachedTimeUTC { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            CachedTimeUTC = "Cached Time Expired";
+            var encodedCachedTimeUTC = await _cache.GetAsync("cachedTimeUTC");
+
+            if (encodedCachedTimeUTC != null)
+            {
+                CachedTimeUTC = Encoding.UTF8.GetString(encodedCachedTimeUTC);
+            }
+        }
+        public async Task<IActionResult> OnPostResetCachedTime()
+        {
+            var currentTimeUTC = DateTime.UtcNow.ToString();
+            byte[] encodedCurrentTimeUTC = Encoding.UTF8.GetBytes(currentTimeUTC);
+            var options = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromSeconds(120));
+            _cache.Set("cachedTimeUTC", encodedCurrentTimeUTC, options);
+
+            return RedirectToPage();
+        }
+    }
+}

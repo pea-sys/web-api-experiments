17f759f64fcacd338af36c508e71cdcced665c61
pea-sys <pea98258@gmail.com>
2022-06-30 21:26:51 +0900

    signalr-js

---
+++ SignalRChat-JS/SignalRChat-JS/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat_JS.Hubs
+{
+    /// <summary>
+    /// ハブはクライアント サーバー通信を処理するハイレベル パイプラインとして機能するクラス
+    /// </summary>
+    public class ChatHub : Hub
+    {
+        /// <summary>
+        /// SendMessage メソッドは、メッセージをすべてのクライアントに送信する
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
